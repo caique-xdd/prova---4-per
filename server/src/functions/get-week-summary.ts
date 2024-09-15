48616e64ed4a7758d5ef440fc2521df69263df6e
caique-xdd <caiquedecarvalho990@gmail.com>
2024-09-15 10:47:07 -0300

    5

--- server/src/functions/get-week-summary.ts
+++ server/src/functions/get-week-summary.ts
@@ -57,6 +57,15 @@ export async function getWeekSummary() {
       .groupBy(goalsCompletedInWeek.completedAtDate)
   )
 
+  type GoalsPerDay = Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    }[]
+  >
+
   const result = await db
     .with(goalsCreatedUpToWeek, goalsCompletedInWeek, goalsCompletedByWeekDay)
     .select({
@@ -69,7 +78,7 @@ export async function getWeekSummary() {
           Number
         ),
 
-      goalsPerDay: sql /*sql*/`
+      goalsPerDay: sql /*sql*/<GoalsPerDay>`
       JSON_OBJECT_AGG(
         ${goalsCompletedByWeekDay.completedAtDate},
         ${goalsCompletedByWeekDay.completions}
@@ -79,6 +88,6 @@ export async function getWeekSummary() {
     .from(goalsCompletedByWeekDay)
 
   return {
-    summary: result,
+    summary: result[0],
   }
 }
--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,14 +1,22 @@
+import { useEffect, useState } from 'react'
+
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { Summery } from './components/summery'
-// import { EmptyGoals } from './components/empty-goals'
+import { EmptyGoals } from './components/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60,
+  })
+
   return (
     <Dialog>
-      {/*<EmptyGoals />*/}
-
-      <Summery />
+      {data?.total && data.total > 0 ? <Summery /> : <EmptyGoals />}
 
       <CreateGoal />
     </Dialog>
